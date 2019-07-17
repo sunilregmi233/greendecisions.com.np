254f7573b3b1c26d2f892687873cba4512e70f8e
sunilregmi233 <sunilregmi223@gmail.com>
2019-07-17 19:58:58 +0545

    using gmail in contact us form

--- custom form/sendemail/views.py
+++ custom form/sendemail/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .forms import ContactForm
 
-# Create your views here.
-def Homepage(request):
+def emailView(request):
     if request.method == 'GET':
         form = ContactForm()
     else:
@@ -20,11 +17,7 @@ def Homepage(request):
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('success')
-    return render(request, "index.html", {'form': form})
-
-
-
-
+    return render(request, "email.html", {'form': form})
 
 def successView(request):
     return HttpResponse('Success! Thank you for your message.')
